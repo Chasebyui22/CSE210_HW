d5b5960899608bc3e5d8f1a60ff5b314e95515b3
unknown <chased.smith1@gmail.com>
2024-04-26 12:39:58 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int a = 93;
+        int aLow = 90;
+        int bHigh = 87;
+        int b = 83;
+        int bLow = 80;
+        int cHigh = 77;
+        int c = 73;
+        int cLow =70;
+        int dHigh = 67;
+        int d = 63;
+        int dLow =60;
+        int f = 59;
+
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        
+        if (int.Parse(percentage) > a)
+        {
+            Console.WriteLine("You have an A");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > aLow)
+        {
+            Console.WriteLine("You have an A -");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > bHigh)
+        {
+            Console.WriteLine("You have an B +");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > b)
+        {
+            Console.WriteLine("You have an B");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > bLow)
+        {
+            Console.WriteLine("You have an B -");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > cHigh)
+        {
+            Console.WriteLine("You have an C +");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > c)
+        {
+            Console.WriteLine("You have an C");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > cLow)
+        {
+            Console.WriteLine("You have an C -");
+            Console.WriteLine("You passed the course");
+        }
+        else if (int.Parse(percentage) > dHigh)
+        {
+            Console.WriteLine("You have an D +");
+            Console.WriteLine("You did not pass the course");
+        }
+        else if (int.Parse(percentage) > d)
+        {
+            Console.WriteLine("You have an D");
+            Console.WriteLine("You did not pass the course");
+        }
+        else if (int.Parse(percentage) > dLow)
+        {
+            Console.WriteLine("You have an D -");
+            Console.WriteLine("You did not pass the course");
+        }
+        else
+        {
+            Console.WriteLine("You have an F");
+            Console.WriteLine("You did not pass the course");
+        }
+    
     }
 }
