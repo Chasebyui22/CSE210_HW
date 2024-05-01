4cd511801289347c479c33c781e6664395abc769
Chase <smi19037@byui.edu>
2024-05-01 00:59:28 -0600

    Worked on Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,7 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string answer = Console.ReadLine();
+        float number = float.Parse(answer);
+        Console.Write("What is your guess? ");
+        string answers = Console.ReadLine();
+        float guess = float.Parse(answers);
+        if (number > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (number < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.Write("You got it!");
+        }
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            answers = Console.ReadLine();
+            guess = float.Parse(answers);
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.Write("You got it!");
+            }
+        } 
     }
 }
-//hello
