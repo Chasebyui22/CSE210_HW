9058c35558b3fe2230a63d37b35020fb2e0575f4
Chase <smi19037@byui.edu>
2024-05-10 15:05:04 -0600

    Add DiceDistribution project and tasks for building it

--- sandbox/Sandbox/Sandbox.cs
+++ sandbox/Sandbox/Sandbox.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Course course1 = new Course();
+        course1._courseCode = "CSE 210";
+        course1._courseName = "Prog. with Classess";
+        course1._creditHours = 2;
+        course1._color = "Green";
+        course1.Display();       
+    } 
+} 
